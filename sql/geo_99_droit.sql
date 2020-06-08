6bfd00154cb619184d58165ad86afe0264e03124
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-06-08 09:43:09 +0200

    add table docs TEPOS
    Ajout des éléments pour l'ajout de document sur les territoires TEPOS

---
+++ sql/geo_99_droit.sql
@@ -0,0 +1,14 @@
+/*GEO V1.0*/
+/*Creation du fichier "00_trace" qui permet de suivre l'évolution du code*/
+/* geo_00_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : Conseil régional Nouvelle-Aquitaine - http://cartographie.nouvelle-aquitaine.fr/ */
+/* Auteur : Tony VINCENT*/
+
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+2020-06-08 : TV / initialisation du code uniquement
+2020-06-08 : TV / initialisation de la structure pour les documents Territoire TEPOS
+2020-06_08 : TV / ajout des droits pour la structure pour les documents Territoire TEPOS
