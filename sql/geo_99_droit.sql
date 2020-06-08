2b3cd6b63048f32222555ee88b3a04491d418b3d
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-06-08 09:46:20 +0200

    add authorized media_territ_tepos
    Ajout des droits pour les éléments territoire TEPOS

--- sql/geo_99_droit.sql
+++ sql/geo_99_droit.sql
@@ -1,14 +1,40 @@
 /*GEO V1.0*/
-/*Creation du fichier "00_trace" qui permet de suivre l'évolution du code*/
-/* geo_00_trace.sql */
+/*Creation du fichier "00_droit" qui permet de suivre l'évolution du code*/
+/* geo_99_droit.sql */
 /*PostGIS*/
 
 /* Propriétaire : Conseil régional Nouvelle-Aquitaine - http://cartographie.nouvelle-aquitaine.fr/ */
 /* Auteur : Tony VINCENT*/
 
 
-/*
-#################################################################### SUIVI CODE SQL ####################################################################
-2020-06-08 : TV / initialisation du code uniquement
-2020-06-08 : TV / initialisation de la structure pour les documents Territoire TEPOS
-2020-06_08 : TV / ajout des droits pour la structure pour les documents Territoire TEPOS
+/--
+GRANT ALL ON SCHEMA upload TO "pre-sig-usr";
+GRANT ALL ON SCHEMA upload TO "pre-sig-ro";
+
+--
+GRANT ALL ON TABLE upload.media_bdd TO "pre-sig-usr";
+GRANT ALL ON TABLE upload.media_bdd TO "pre-sig-ro";
+
+GRANT ALL ON SEQUENCE upload.media_bdd_id_seq TO "pre-sig-usr";
+GRANT ALL ON SEQUENCE upload.media_bdd_id_seq TO "pre-sig-ro";
+
+--
+GRANT ALL ON TABLE upload.media_rep TO "pre-sig-usr";
+GRANT ALL ON TABLE upload.media_rep TO "pre-sig-ro";
+
+--
+GRANT ALL ON TABLE upload.media_signaletique TO "pre-sig-ro";
+GRANT ALL ON SEQUENCE upload.media_signaletique_id_seq TO "pre-sig-ro";
+
+--
+GRANT ALL ON TABLE upload.media_dsi_localisation_site TO "pre-sig-ro";
+GRANT ALL ON SEQUENCE upload.media_dsi_localisation_site_id_seq TO "pre-sig-ro";
+
+-- Tables : upload.media_dsi_localisation_site
+GRANT ALL ON TABLE upload.media_dsi_localisation_site TO "pre-sig-ro";
+GRANT ALL ON SEQUENCE upload.media_dsi_localisation_site_id_seq TO "pre-sig-ro";
+
+
+-- Tables : upload.media_terri_tepos
+GRANT ALL ON TABLE upload.media_terri_tepos TO "pre-sig-ro";
+GRANT ALL ON SEQUENCE upload.media_terri_tepos_id_seq TO "pre-sig-ro";
